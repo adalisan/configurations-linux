785fab8b7a9a8078eedaca8ec5a0addcd915655e
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2017-11-28 01:33:46 -0500

    remove some installs

--- software-install.sh
+++ software-install.sh
@@ -4,7 +4,6 @@ set -e
 printf "Installing recommended softwares...\n"
 
 
-# Updating apt to refresh repos
 sudo apt-get update
 
 globals=(
@@ -30,21 +29,3 @@ apt-install
 # Install PIP for python
 easy_install pip
 
-read -p "Would you like to install Node.js? [Y/N] " -n 1;
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	# Instal NVM to install Node.js
-	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
-	echo "source ~/.nvm/nvm.sh" >> "${HOME}/.bash_profile"
-	source ~/.bash_profile
-	nvm install stable
-	source ~/.bash_profile
-	# Fix the node.js and node issue in Ubuntu
-	ln -s /usr/bin/nodejs /bin/node
-fi;
-
-read -p "Would you like to install NPM globals? [Y/N] " -n 1;
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	sudo bash "/root/.npm_globals.sh"
-fi;
