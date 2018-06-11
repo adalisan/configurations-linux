b11dfe38fa8e0f43d3a5269e9207516ec656757f
Sancar Adali <sadali+git@gmail.com>
2018-06-11 01:04:36 -0400

    configure bash setttings to my liking

--- configure-bash-it.sh
+++ configure-bash-it.sh
@@ -7,14 +7,15 @@ printf "Configuring Bash-it installation...\n"
 # Path to the bash it configuration
 export BASH_IT="$HOME/.bash_it"
 
-bash-it enable alias all
-bash-it enable plugin all
-bash-it enable completion all
+
+bash-it enable plugin alias-completion tmux ssh projects  history extract explain dirs compress xterm
+
+bash-it enable alias apt curl docker general git homebrew tmux
+bash-it enable completion bash-it	brew conda	defaults dirs docker	docker-compose	docker-machine git hub pip	projects	ssh system	tmux
 
 bash-it disable plugin chruby
 bash-it disable plugin chruby-auto
 bash-it disable plugin postgres
 bash-it disable plugin z
 bash-it disable plugin todo
-bash-it disable completion conda
 bash-it disable alias emacs
