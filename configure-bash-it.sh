7a2d67665c23e054f4225234ac313983801ccf8d
Sancar Adali <sadali@bbn.com>
2018-07-31 16:24:13 -0400

    remove bash-it due to hangup in autocompletion

--- configure-bash-it.sh
+++ configure-bash-it.sh
@@ -5,13 +5,19 @@ source ~/.bash_profile
 
 printf "Configuring Bash-it installation...\n"
 # Path to the bash it configuration
-export BASH_IT="$HOME/.bash_it"
+BASH_IT="$HOME/.bash_it"
+
+if [[ -L "/home/sadali/.bash_it_local" ]] && [[ -e "/home/sadali/.bash_it_local" ]] ; then
+  BASH_IT="/home/sadali/.bash_it_local"
+fi
+
+export BASH_IT
 
 
 bash-it enable plugin base alias-completion tmux ssh projects  history extract explain dirs compress xterm
 
 bash-it enable alias apt curl docker general git homebrew tmux
-bash-it enable completion bash-it	brew conda	defaults dirs docker	docker-compose	docker-machine git hub pip	projects	ssh system	tmux
+bash-it enable completion brew conda	defaults dirs docker docker-compose docker-machine git hub pip	projects ssh system	tmux
 
 bash-it disable plugin chruby
 bash-it disable plugin chruby-auto
